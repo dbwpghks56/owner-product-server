f22af7147893d3f2a935ce740c038eadd5326874
wpghks <wpghks56@naver.com>
2024-02-23 10:50:50 +0900

    [Feature] product controller

--- src/main/java/com/owner/product/domain/product/controller/ProductController.java
+++ src/main/java/com/owner/product/domain/product/controller/ProductController.java
@@ -1,13 +1,48 @@
 package com.owner.product.domain.product.controller;
 
+import com.owner.product.domain.product.dto.request.ProductRequestDto;
+import com.owner.product.domain.product.service.ProductService;
+import com.owner.product.global.responses.success.codes.ProductSuccessCode;
+import com.owner.product.global.responses.success.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping()
+    public ResponseEntity<SuccessResponse> save(
+            @RequestBody ProductRequestDto.Save saveRequest
+            ) {
+        return SuccessResponse.toResponseEntity(
+                ProductSuccessCode.SAVE,
+                productService.createProduct(saveRequest)
+        );
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<SuccessResponse> findOne(
+            @PathVariable(name = "productId") Long productId
+    ) {
+        return SuccessResponse.toResponseEntity(
+                ProductSuccessCode.FIND,
+                productService.findDetail(productId)
+        );
+    }
+
+    @GetMapping()
+    public ResponseEntity<SuccessResponse> findWithPaging(
+            @ModelAttribute ProductRequestDto.Find findRequest
+    ) {
+        return SuccessResponse.toResponseEntity(
+                ProductSuccessCode.FIND,
+                productService.findWhole(findRequest)
+        );
+    }
 }
